d305bd275e05fcf76a02d01194ffdfec993d486e
Islandvibess <doublebobr@gmail.com>
2025-04-22 15:05:38 +0500

    load 4 files

---
+++ site.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll('.section');
+
+const showSection = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < triggerBottom) {
+      section.classList.add('visible');
+    }
+  });
+};
+
+window.addEventListener('scroll', showSection);
+window.addEventListener('load', showSection);
